7b702e538f775b900e90224e75aaa1df16e43a5c
Foo0s <62848145+Foo0s@users.noreply.github.com>
2022-11-27 21:21:00 +0300

    Update parser.py

--- Bot_pogoda/parser.py
+++ Bot_pogoda/parser.py
@@ -10,12 +10,3 @@ def parser():
     dop = soup.find("div", class_="information__content__additional information__content__additional_first").get_text().lstrip().rstrip()
     answer = f"День: {day}\nПогода: {gradys}\nДругая информация: {oshyt}\nДоп: {dop}"
     return answer
-
-def probki():
-    url = "http://www.probki-online.ru/probki-online.php?city=rostov-na-donu"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
-    info = soup.find('ymaps', class_="ymaps-2-1-79-float-button-text").get_text()
-    answer = f"Пробки(балл): {info}"
-    return answer
-print(probki())
